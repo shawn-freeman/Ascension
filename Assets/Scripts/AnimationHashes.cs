93ced53112ef99f4dd2fe55d37292b2880de7a1f
ShawnFreeman-PC <shawn.freman@gmail.com>
2016-11-13 13:16:48 -0500

    Initial Unity Project checkin

---
+++ Assets/Scripts/AnimationHashes.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationHashes {
+
+    public static int MENU_SwingViewToFront = Animator.StringToHash("SwingViewToFront");
+    public static int MENU_SwingFrontToView = Animator.StringToHash("SwingFrontToView");
+    public static int MENU_InitiallyInActive = Animator.StringToHash("InitiallyInActive");
+}
---
+++ Assets/Scripts/LoginButton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class LoginButton : MonoBehaviour
+{
+    private string BaseApiAddress = "http://192.168.1.169:8080/api/Login?";
+
+    public InputField Username = null;
+    public InputField Password = null;
+    public Text ErrorText = null;
+    public GameObject LoadingIndicator = null;
+
+    public GameObject MenuPanel = null;
+    private Animator MenuAnimator;
+
+    public void Start()
+    {
+        MenuAnimator = MenuPanel.GetComponent<Animator>();
+    }
+
+    public void OnClick()
+    {
+        string ApiAddress = string.Format("{0}username={1}&password={2}", BaseApiAddress, Username.text, Password.text);
+        WWW www = new WWW(ApiAddress);
+
+        LoadingIndicator.SetActive(true);
+        StartCoroutine(AttemptLogin(www));
+    }
+
+    public IEnumerator AttemptLogin(WWW www)
+    {
+        yield return www;
+
+        int userId;
+        if (www.error == null && Int32.TryParse(www.text, out userId))
+        {
+            if (userId != -1)
+            {
+                ErrorText.text = "Login Successful for: " + www.text;
+                MenuAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
+            }
+            else
+            {
+                ErrorText.text = "Username and/or Password was entered incorrect.";
+            }
+        }else
+        {
+            ErrorText.text = "Username and/or Password was entered incorrect.";
+        }
+        LoadingIndicator.SetActive(false);
+    }
+}
