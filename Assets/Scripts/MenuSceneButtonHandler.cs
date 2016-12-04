6e5c4d5b4cd6e87f4454c267597fd11f4df4cfe7
ShawnFreeman <shawn.david.freeman@outlook.com>
2016-12-03 19:35:28 -0500

    Added account creation menu with logic wired up

--- Assets/Scripts/MenuSceneButtonHandler.cs
+++ Assets/Scripts/MenuSceneButtonHandler.cs
@@ -13,9 +13,11 @@ public class MenuSceneButtonHandler : MonoBehaviour
     private const string LOGIN = "Login";
     private const string MAIN_MENU = "MainMenu";
     private const string CHANGE_PASSWORD = "ChangePassword";
+    private const string CREATE_ACCOUNT = "CreateAccount";
 
     public InputField Username = null;
     public InputField Password = null;
+    public InputField Email = null;
     public InputField ChangePassword = null;
     public InputField ConfirmPassword = null;
 
@@ -32,6 +34,9 @@ public class MenuSceneButtonHandler : MonoBehaviour
     public GameObject ChangePasswordPanel = null;
     private Animator ChangePasswordPanelAnimator;
 
+    public GameObject CreateAccountPanel = null;
+    private Animator CreateAccountPanelAnimator;
+
     public HttpHandler httpHandler;
 
     public void Start()
@@ -40,7 +45,75 @@ public class MenuSceneButtonHandler : MonoBehaviour
         if (LoginPanel != null) LoginPanelAnimator = LoginPanel.GetComponent<Animator>();
         if(MainMenuPanel != null) MainMenuPanelAnimator = MainMenuPanel.GetComponent<Animator>();
         if(ChangePasswordPanel != null) ChangePasswordPanelAnimator = ChangePasswordPanel.GetComponent<Animator>();
+        if(CreateAccountPanel != null) CreateAccountPanelAnimator = CreateAccountPanel.GetComponent<Animator>();
     }
+
+    #region MenuNavigation
+
+    public void TransToLogin(string from)
+    {
+        Username.text = string.Empty;
+        Password.text = string.Empty;
+        ErrorText.text = string.Empty;
+        switch (from)
+        {
+            case MAIN_MENU:
+                LoginPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
+                MainMenuPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
+                break;
+            case CREATE_ACCOUNT:
+                LoginPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
+                CreateAccountPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
+                break;
+        }
+    }
+
+    public void TransToMainMenu(string from)
+    {
+        switch (from)
+        {
+            case LOGIN:
+                LoginPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
+                MainMenuPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
+                break;
+            case CHANGE_PASSWORD:
+                MainMenuPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
+                ChangePasswordPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
+                break;
+        }
+    }
+
+    public void TransToChangePassword(string from)
+    {
+        switch (from)
+        {
+            case MAIN_MENU:
+                ChangePassword.text = string.Empty;
+                ConfirmPassword.text = string.Empty;
+                MainMenuPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
+                ChangePasswordPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
+                break;
+        }
+    }
+
+    public void TransToCreateAccount(string from)
+    {
+        Username.text = string.Empty;
+        Password.text = string.Empty;
+        Email.text = string.Empty;
+        ErrorText.text = string.Empty;
+        switch (from)
+        {
+            case LOGIN:
+                LoginPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
+                CreateAccountPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
+                break;
+        }
+    }
+
+    #endregion
+
+    #region Login
 
     public void OnLoginClicked()
     {
@@ -73,48 +146,9 @@ public class MenuSceneButtonHandler : MonoBehaviour
         TransToLogin(MAIN_MENU);
     }
 
-    private void TransToLogin(string from)
-    {
-        switch (from)
-        {
-            case MAIN_MENU:
-                Username.text = string.Empty;
-                Password.text = string.Empty;
-                ErrorText.text = string.Empty;
-                LoginPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
-                MainMenuPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
-                break;
-        }
-    }
+    #endregion
 
-    private void TransToMainMenu(string from)
-    {
-        switch (from)
-        {
-            case LOGIN:
-                LoginPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
-                MainMenuPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
-                break;
-            case CHANGE_PASSWORD:
-                MainMenuPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
-                ChangePasswordPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
-                break;
-        }
-    }
-
-    private void TransToChangePassword(string from)
-    {
-        switch (from)
-        {
-            case MAIN_MENU:
-                ChangePassword.text = string.Empty;
-                ConfirmPassword.text = string.Empty;
-                MainMenuPanelAnimator.SetBool(AnimationHashes.MENU_SwingViewToFront, true);
-                ChangePasswordPanelAnimator.SetBool(AnimationHashes.MENU_SwingFrontToView, true);
-                break;
-        }
-    }
-
+    #region ChangePassword
     public void OnChangePasswordClicked()
     {
         TransToChangePassword(MAIN_MENU);
@@ -153,5 +187,60 @@ public class MenuSceneButtonHandler : MonoBehaviour
         }
     }
 
+    #endregion
 
+    #region CreateAccount
+
+    public void OnCreateAccountClicked()
+    {
+        TransToCreateAccount(LOGIN);
+    }
+
+    public void OnAttemptCreateAccountClicked()
+    {
+        if (string.IsNullOrEmpty(Username.text) || 
+            string.IsNullOrEmpty(Email.text) || 
+            string.IsNullOrEmpty(Password.text) || 
+            string.IsNullOrEmpty(ConfirmPassword.text)) {
+            ErrorText.text = "All fields are required.";
+            return;
+        }
+        //client side validation
+        ErrorText.text = "";
+        if (Password.text != ConfirmPassword.text)
+        {
+            ErrorText.text = "Passwords does not match.";
+            return;
+        }
+
+        LoadingIndicator.SetActive(true);
+
+        var accountRequest = new CreateAccountRequest();
+        accountRequest.Username = Username.text;
+        accountRequest.Email = Email.text;
+        accountRequest.Password = Password.text;
+
+        httpHandler.POST<CreateAccountRequest, ReturnResult<bool>>(ControllerTypes.Account, accountRequest, OnAttemptCreateAccountResponse);
+    }
+
+    public void OnAttemptCreateAccountResponse(ReturnResult<bool> response)
+    {
+        LoadingIndicator.SetActive(false);
+        if (!response.HasError)
+        {
+            if (response.Value)
+            {
+                TransToLogin(CREATE_ACCOUNT);
+            }else
+            {
+                ErrorText.text = response.ExceptionMessage;
+            }
+            
+        }else
+        {
+            ErrorText.text = response.ExceptionMessage;
+        }
+    }
+
+    #endregion
 }
