650cd8f185c57d18aaeb2fc86e4a1e8534c8e189
ShawnFreeman <shawn.david.freeman@outlook.com>
2017-01-10 00:38:46 -0500

    Created singleton of controller and added keydown callback event handling from controller

--- Assets/Resources/Scripts/Controller.cs
+++ Assets/Resources/Scripts/Controller.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
 
 public class Controller : MonoBehaviour {
 	
@@ -7,13 +9,59 @@ public class Controller : MonoBehaviour {
 	public static Vector3 mousePosition;
 	public static float compassDegree;
 	public bool isMobile;
-	// Use this for initialization
-	void Start () 
+
+    private Dictionary<string, List<Action<Vector3>>> _inputCallbacks;
+
+    private static Controller _instance;
+    public static Controller _Instance
+    {
+        get
+        {
+            //check that the singleton has not been set yet
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<Controller>();
+
+                //no object with this component exists, throw an error
+                if (_instance == null)
+                {
+                    throw new Exception("Could not find a gameobject with the Controller component.");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        _inputCallbacks = new Dictionary<string, List<Action<Vector3>>>();
+
+        if (Input.acceleration != null) isMobile = true;
+
+        if (isMobile) Input.location.Start();
+    }
+
+    // Use this for initialization
+    void Start () 
 	{
-		if(Input.acceleration != null) isMobile = true;
 		
-		if(isMobile) Input.location.Start();
 	}
+
+    public void RegisterInput<T>(Action<Vector3> inputCallback, KeyCode keyCode)
+    {
+        var strKey = keyCode.ToString();
+        List<Action<Vector3>> temp;
+
+        if (!_inputCallbacks.ContainsKey(strKey))
+        {
+            _inputCallbacks[strKey] = new List<Action<Vector3>>();
+        }
+
+        temp = _inputCallbacks[strKey];
+        temp.Add(inputCallback);
+        
+    }
 	
 	// Update is called once per frame
 	void Update () 
@@ -34,6 +82,20 @@ public class Controller : MonoBehaviour {
 		}else{
 			//keyboard movement
 			inputMovement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+            var keys = _inputCallbacks.Keys;
+            foreach (var key in keys)
+            {
+                if (Input.GetKeyDown(key.ToLower()))
+                {
+                    var callbacks = _inputCallbacks[key];
+
+                    foreach (var callback in callbacks)
+                    {
+                        callback(inputMovement);
+                    }
+                }
+            }
 		}
 	}
 }
